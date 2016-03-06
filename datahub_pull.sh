fdfa9b833ec766905e7de5295d1fe394b35bb3e7
yepeng <yepeng_xj@163.com>
2016-03-06 11:37:45 +0800

    Update datahub_pull.sh

--- datahub_pull.sh
+++ datahub_pull.sh
@@ -1,9 +1,21 @@
-#!/usr/bin/expect -f  
+#!/usr/bin/expect -f
+
+set user [lindex $argv 0]
+set password [lindex $argv 1]
+set repo_tag [lindex $argv 2]
+set local_dp [lindex $argv 3]
+
+set timeout 10
+spawn datahub login
+expect "login*"
+send "$user\r"
+expect "password:*"
+send "$password\r"
+
 
  set timeout 10  
- spawn datahub pull $3 $4
+ spawn datahub pull $repo_tag $local_dp
  expect "login*"  
- send $1"\r"  
+ send $user"\r"  
  expect "password:*"  
- send $2"\r"  
- 
+ send $password"\r"
